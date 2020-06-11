90ae8a4af72195bc00ffbac763a29b31da0645c1
Dave Schwartz <d.m.schwartz708@gmail.com>
2020-06-11 14:40:57 -0400

    Corrected various errors

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+//modified using
+//PHP 7.3.16 (cli) (built: Mar 17 2020 13:32:34) ( NTS MSVC15 (Visual C++ 2017) x64 )
+//Copyright (c) 1997-2018 The PHP Group
+//Zend Engine v3.3.16, Copyright (c) 1998-2018 Zend Technologies
 
 class Interview
 {
-	public $title = 'Interview test';
+	//$title needs to be static
+	public static $title = 'Interview test';
 }
 
 $lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
@@ -15,7 +20,8 @@ $people = array(
 	array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
 );
 
-$person = $_POST['person'];
+//added check to ensure $_POST['person'] is set before referencing it
+$person = isset($_POST['person']) ? $_POST['person'] : null;
 
 ?>
 
@@ -30,12 +36,16 @@ $person = $_POST['person'];
 </head>
 <body>
 
-	<h1><?=Interview::$title;?></h1>
+	<!-- $title needed to be changed to static in class Interview and added htmlentities -->
+	<h1><?=htmlentities(Interview::$title);?></h1>
 
 	<?php
 	// Print 10 times
-	for ($i=10; $i<0; $i++) {
-		echo '<p>'+$lipsum+'</p>';
+	//loop in wrong order should be 0 to 10
+	for ($i=0; $i<10; $i++) {
+		//concatenation was incorrect changed to dots
+		//added htmlentities for formatting
+		echo '<p>' . htmlentities($lipsum) . '</p>';
 	}
 	?>
 
@@ -43,7 +53,8 @@ $person = $_POST['person'];
 	<hr>
 
 
-	<form method="get" action="<?=$_SERVER['REQUEST_URI'];?>">
+	<!-- change method to post -->
+	<form method="post" action="<?=$_SERVER['REQUEST_URI'];?>">
 		<p><label for="firstName">First name</label> <input type="text" name="person[first_name]" id="firstName"></p>
 		<p><label for="lastName">Last name</label> <input type="text" name="person[last_name]" id="lastName"></p>
 		<p><label for="email">Email</label> <input type="text" name="person[email]" id="email"></p>
@@ -51,7 +62,8 @@ $person = $_POST['person'];
 	</form>
 
 	<?php if ($person): ?>
-		<p><strong>Person</strong> <?=$person['first_name'];?>, <?=$person['last_name'];?>, <?=$person['email'];?></p>
+		<!-- addded htmlentities for formatting -->
+		<p><strong>Person</strong> <?=htmlentities($person['first_name']);?>, <?=htmlentities($person['last_name']);?>, <?=htmlentities($person['email']);?></p>
 	<?php endif; ?>
 
 
@@ -69,9 +81,11 @@ $person = $_POST['person'];
 		<tbody>
 			<?php foreach ($people as $person): ?>
 				<tr>
-					<td><?=$person->first_name;?></td>
-					<td><?=$person->last_name;?></td>
-					<td><?=$person->email;?></td>
+					<!-- $person is not an object so it should be accessed by associated references -->
+					<!-- added htmlentities for formatting -->
+					<td><?=htmlentities($person['first_name']);?></td>
+					<td><?=htmlentities($person['last_name']);?></td>
+					<td><?=htmlentities($person['email']);?></td>
 				</tr>
 			<?php endforeach; ?>
 		</tbody>
